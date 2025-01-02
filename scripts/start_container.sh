4d111d707e9b2a9160ac46534261689a1e8501fa
Isuru Lakshan <145681127+LakshanDerren@users.noreply.github.com>
2025-01-02 10:39:20 +0530

    PULLPUSHMIS

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push lakshandarrin/simple-python-app
+docker pull lakshandarrin/simple-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 lakshandarrin/simple-python-app
